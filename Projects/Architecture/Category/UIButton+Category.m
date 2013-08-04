4553e5e91110a42faa638d61ceb2baed1a9893c4
Gary <p709723778@126.com>
2013-08-04 20:43:10 +0800

    注释改善

--- Projects/Architecture/Category/UIButton+Category.m
+++ Projects/Architecture/Category/UIButton+Category.m
@@ -13,6 +13,8 @@
 
 //点击按钮通用声音 重写了点击结束事件  然后调用基类事件
 //此处要注意  caf格式的声音文件只传文件名即可,无需后缀名,其他格式的要带后缀名
+//此处扩展的话  可以自定义声音  还有iPad兼容不同声音,后续需要改
+
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [[SoundManager sharedManager] playSound:@"tap.aif" looping:NO];
